b88fcc76b478cce1dc988aaa1847cf3f00540e71
Arnaud Gelas <arnaud.gelas@sensefly.com>
2019-03-26 21:26:15 +0100

    Add simple cmake files

---
+++ dummy-config.cmake
@@ -0,0 +1,3 @@
+message("here: ${CMAKE_CURRENT_LIST_FILE}")
+
+set(dummy_MODULE_PATH ${CMAKE_CURRENT_LISTDIR})
---
+++ my-super-cmake-file.cmake
@@ -0,0 +1,6 @@
+message("file: ${CMAKE_CURRENT_LIST_FILE}")
+
+macro(do_some_incredible_stuff)
+	message("print something very smart")
+	message("perform some incredible stuff")
+endmacro()
