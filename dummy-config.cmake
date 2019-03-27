e9779cf6ad60c8612bf9903f51957528b3ca95d8
Arnaud Gelas <arnaud.gelas@sensefly.com>
2019-03-27 15:08:34 +0100

    Fix dummy-config and print more variables

--- dummy-config.cmake
+++ dummy-config.cmake
@@ -1,3 +1,4 @@
 message("here: ${CMAKE_CURRENT_LIST_FILE}")
 
-set(dummy_MODULE_PATH ${CMAKE_CURRENT_LISTDIR})
+set(dummy_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+message("dummy_MODULE_PATH ${dummy_MODULE_PATH}")
